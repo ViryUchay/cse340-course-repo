1d1634ce05b97ef07bf58328e91d3326ccfb111b
viryuchay <ezevive@gmail.comgit config user.name>
2026-09-18 03:39:08 +0100

    Configure production database connection URI and SSL

--- site/src/database.js
+++ site/src/database.js
@@ -1,15 +1,11 @@
 import pg from 'pg';
-import dotenv from 'dotenv';
-
-dotenv.config();
-
 const { Pool } = pg;
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false,
-    },
+    ssl: isProduction ? { rejectUnauthorized: false } : false
 });
 
 export default pool;
