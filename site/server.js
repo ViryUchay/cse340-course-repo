b46bdfa1fc0de7397db4469a7d72e366bd81f89c
viryuchay <ezevive@gmail.comgit config user.name>
2026-09-21 13:27:56 +0100

    rev

--- site/server.js
+++ site/server.js
@@ -20,6 +20,20 @@ const asyncHandler = (handler) => (req, res, next) => {
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+// Middleware to log all incoming requests
+app.use((req, res, next) => {
+    if (NODE_ENV === 'development') {
+        console.log(`${req.method} ${req.url}`);
+    }
+    next(); // Pass control to the next middleware or route
+});
+
+// Middleware to make NODE_ENV available to all templates
+app.use((req, res, next) => {
+    res.locals.NODE_ENV = NODE_ENV;
+    next();
+});
+
 // Static middleware to serve the public folder (css, images, client js)
 app.use(express.static(path.join(__dirname, 'public')));
 
